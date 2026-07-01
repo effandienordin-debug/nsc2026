56cfa23966097ba949218fc3575c45c95cba48e1
InsightNest <effandie.nordin@gmail.com>
2026-07-01 17:27:30 +0800

    Remove unnecessary blank line in admin_logic.py

--- admin_logic.py
+++ admin_logic.py
@@ -201,7 +201,6 @@ def render_management(menu, engine, hash_password, delete_item):
         st.divider()
         
         tab1, tab2 = st.tabs(["📋 Registered Teams", "👥 Group Assignments (Juries)"])
-        
         with tab1:
     apps_df = pd.read_sql("SELECT * FROM teams ORDER BY group_category ASC, team_id ASC", engine)
     
